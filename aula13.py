c2b37a33936c63adf7665134ef20378a01dd44c9
Thiago Copetti <thiago.rc2009@hotmail.com>
2025-06-26 20:52:05 -0300

    aula

---
+++ aula13.py
@@ -0,0 +1,8 @@
+nome = 'Thiago'
+altura = 1.80
+peso = 80.5
+imc = peso / (altura*2)
+
+print(nome, 'tem', altura, 'de altura e pesa', peso, 'kg.')
+print('Seu Imc é ', imc)
+print(f'{nome} tem {altura:.2f} de altura e pesa {peso:.1f} kg.')
