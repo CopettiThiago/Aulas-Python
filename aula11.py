f873558ca39aeb68b0ee94f740d65616f73673b1
Thiago Copetti <thiago.rc2009@hotmail.com>
2025-06-26 20:27:46 -0300

    aulas

---
+++ aula11.py
@@ -0,0 +1,7 @@
+#1. (n + n) parenteses
+#2. ** potenciacao
+#3. * / // % multiplicacao divisao modulo
+#4. + -
+
+conta_1 = 1 + 1 ** 5 + 5  # 7
+print(conta_1)
---
+++ aula12.py
@@ -0,0 +1,7 @@
+nome = 'Thiago'
+altura = 1.80
+peso = 80.5
+imc = peso / (altura*2)
+
+print(nome, 'tem', altura, 'de altura e pesa', peso, 'kg.')
+print('Seu Imc é ', imc)
