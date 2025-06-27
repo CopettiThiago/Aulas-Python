30f35553796ce6a63083db06db62884876acc634
Thiago Copetti <thiago.rc2009@hotmail.com>
2025-06-27 18:28:37 -0300

    aulas

---
+++ aula17.py
@@ -0,0 +1,18 @@
+# if / elif / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Este é o código da condicao 1')
+elif condicao2:
+    print('Este é o código da condicao 2')
+elif condicao3:
+    print('Este é o código da condicao 3')
+elif condicao4:
+    print('Este é o código da condicao 4')
+else:
+    print('Nenhuma condicao foi satisfeita')
